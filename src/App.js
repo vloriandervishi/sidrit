b9ef4d7b5efeecf33e271e3935a1a754007fa739
vlorian <vlorian.dervishi@gmail.com>
2023-07-30 23:01:10 -0400

    import files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Content from "./content";
+import Footer from "./footer";
+import { useState } from "react";
+import AddItem from "./Additem";
+import SearchItem from "./SearchItem";
 
 function App() {
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppingCart"))
+  );
+
+  const [newItems, newSetItems] = useState("");
+  const [search, setSearch] = useState("");
+  const setandSaveItems = (n) => {
+    setItems(n);
+    localStorage.setItem("shoppingCart", JSON.stringify(n));
+  };
+  const addItems = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const xnewItems = { id, checked: false, item };
+    const listItems = [...items, xnewItems];
+    setandSaveItems(listItems);
+  };
+  const handleCheck = (id) => {
+    const listItem = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setandSaveItems(listItem);
+  };
+  const handleDelete = (id) => {
+    const listitem = items.filter((item) => item.id !== id);
+    setandSaveItems(listitem);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItems) return;
+    addItems(newItems);
+    newSetItems("");
+    console.log("submited");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Grocery List" />
+      <AddItem
+        newItems={newItems}
+        newSetItems={newSetItems}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer />
     </div>
   );
 }
